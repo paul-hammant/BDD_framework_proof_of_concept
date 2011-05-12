cbadf1168c890c2c5b441ebb24d60d58391420dc
Paul Hammant <paul@hammant.org>
2011-05-12 14:40:34 -0500

    covereage drop, reversed

--- src/test/java/newjb/core/ReflectionStepTest.java
+++ src/test/java/newjb/core/ReflectionStepTest.java
@@ -17,33 +17,45 @@ public class ReflectionStepTest {
     @Test
     public void shouldPerformPassingSteps() {
         stepFactory.add(WillPass.METHOD, new WillPass(journal));
-        Step step = new ReflectionStep(WillPass.METHOD, new String[0], "didPass");
+        Step step = new ReflectionStep(WillPass.METHOD, new String[0], "didItPass?");
         step.perform(stepFactory, monitor);
-        assertThat(journal.toString().trim(), is(equalTo("willPass didPass✓")));
+        assertThat(journal.toString().trim(), is(equalTo("willPass didItPass?✓")));
     }
 
     @Test
-    public void shouldThrowOnFailingSteps() {
+    public void shouldHandleFailingSteps() {
         stepFactory.add(WillThrow.METHOD, new WillThrow());
-        Step step = new ReflectionStep(WillThrow.METHOD, new String[0], "didPass");
-        try {
-            step.perform(stepFactory, monitor);
-        } catch (RuntimeException e) {
-            assertThat(e.getMessage(), is(equalTo("boo hoo!")));
-            assertThat(monitor.toString(), is(equalTo("didPass✗")));
-        }
+        Step step = new ReflectionStep(WillThrow.METHOD, new String[0], "didItPass?");
+        boolean foo = step.perform(stepFactory, monitor);
+        assertThat(foo, is(false));
+        assertThat(monitor.eventString(), equalTo("didItPass?(throw RTE!)"));
     }
 
     @Test
-    public void shouldThrowOnAssertingSteps() {
+    public void shouldHandleAssertingSteps() {
         stepFactory.add(WillAssert.METHOD, new WillAssert());
-        Step step = new ReflectionStep(WillAssert.METHOD, new String[0], "didPass");
-        try {
-            step.perform(stepFactory, monitor);
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), is(equalTo("boo hoo!")));
-            assertThat(monitor.toString(), is(equalTo("didPass✗")));
-        }
+        Step step = new ReflectionStep(WillAssert.METHOD, new String[0], "didItPass?");
+        boolean foo = step.perform(stepFactory, monitor);
+        assertThat(foo, is(false));
+        assertThat(monitor.eventString(), equalTo("didItPass?(throw AE!)"));
+    }
+
+    @Test
+    public void shouldHandleErroringSteps() {
+        stepFactory.add(WillError.METHOD, new WillError());
+        Step step = new ReflectionStep(WillError.METHOD, new String[0], "didItPass?");
+        boolean foo = step.perform(stepFactory, monitor);
+        assertThat(foo, is(false));
+        assertThat(monitor.eventString(), equalTo("didItPass?(java.lang.OutOfMemoryError: throw OoME!)"));
+    }
+
+    @Test
+    public void shouldHandleInvalidStepMethod() {
+        stepFactory.add(Invalid.METHOD, new Invalid());
+        Step step = new ReflectionStep(Invalid.METHOD, new String[0], "didItPass?");
+        boolean foo = step.perform(stepFactory, monitor);
+        assertThat(foo, is(false));
+        assertThat(monitor.eventString().contains("IllegalAccessException"), is(true));
     }
 
     public static class WillPass {
@@ -62,14 +74,28 @@ public class ReflectionStepTest {
     public static class WillThrow {
         static final Method METHOD = WillThrow.class.getDeclaredMethods()[0];
         public void willThrow() {
-            throw new RuntimeException("boo hoo!");
+            throw new RuntimeException("throw RTE!");
+        }
+    }
+
+    public static class Invalid {
+        static final Method METHOD = Invalid.class.getDeclaredMethods()[0];
+        private void notAvailable() {
+            System.exit(10); // never called
+        }
+    }
+
+    public static class WillError {
+        static final Method METHOD = WillError.class.getDeclaredMethods()[0];
+        public void willError() {
+            throw new OutOfMemoryError("throw OoME!");
         }
     }
 
     public static class WillAssert {
         static final Method METHOD = WillAssert.class.getDeclaredMethods()[0];
         public void willAssert() {
-            throw new AssertionError("boo hoo!");
+            throw new AssertionError("throw AE!");
         }
     }
 
